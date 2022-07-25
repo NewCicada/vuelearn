ba169415f80bcd4c80b4771aa7ee42dc5daf6584
Cicada <1547598968@qq.com>
2022-07-25 16:14:46 +0800

    单文件组件

---
+++ vue_basic/19.单文件组件/Student.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学生姓名:{{ name }}</h2>
+        <h2>学校地址:{{ age }}</h2>
+    </div>
+</template>
+<script>
+// 组件交互相关的代码(数据、方法等等)e
+export default {
+    name: 'Student',
+    data() {
+        return {
+            schoolName: '张三',
+            address: '18',
+        }
+    },
+}
+</script>
---
+++ vue_basic/19.单文件组件/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Student></Student>
+        <School></School>
+    </div>
+</template>
+
+<script>
+// 引入组件
+import Student from './Student.vue'
+import School from './School.vue'
+export default {
+    name: 'App',
+    components: {
+        School,
+        Student,
+    },
+}
+</script>
+
+<style></style>
