86b06632f6a93477b2b8219543106ef06e01a3cc
Cicada <1547598968@qq.com>
2022-07-26 10:03:59 +0800

    分析脚手架

---
+++ vue_show/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1 v-text="msg" ref="title"></h1>
+        <button ref="btn" @click="showDOM">点我输出上方的DOM元素</button>
+        <School ref="sch" />
+        <School />
+        <School />
+    </div>
+</template>
+
+<script>
+import School from './components/School.vue'
+export default {
+    name: 'App',
+    components: { School },
+    data() {
+        return {
+            msg: '欢迎学习Vue',
+        }
+    },
+    methods: {
+        showDOM() {
+            console.log(this.$refs.title) //真实DOM元素
+            console.log(this.$refs.btn) // 真实DWOM元素
+            console.log(this.$refs.sch) // School组建的实例对象(vc)
+        },
+    },
+}
+</script>
