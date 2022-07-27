75d45fc26cc054e805ba717b69ec362cef2bb248
Cicada <1547598968@qq.com>
2022-07-27 10:13:11 +0800

    自定义事件

---
+++ vue_show/10_src_自定义事件/components/School.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="demo">
+        <h2>学校名称:{{ name }}</h2>
+        <h2>学校地址:{{ address }}</h2>
+        <button @click="sendSchoolName">把学校名给APP</button>
+    </div>
+</template>
+
+<script>
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'School',
+    props: ['getSchoolName'],
+    data() {
+        console.log(this)
+        return {
+            name: 'HuiZho',
+            address: '惠州开发大学',
+        }
+    },
+    methods: {
+        sendSchoolName() {
+            this.getSchoolName(this.name)
+        },
+    },
+    props: ['getSchoolName'],
+}
+</script>
+<style scoped>
+/*scoped代表局部的*/
+.demo {
+    background: skyblue;
+    padding: 5px;
+}
+</style>
