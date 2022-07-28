f0c2617cd12c9e27da155c3bcb2a68d68eb8d677
Cicada <1547598968@qq.com>
2022-07-28 17:32:44 +0800

    Todolist自定义事件版本

--- vue_show/11_src_Todolist自定义事件版本/main.js
+++ vue_show/11_src_Todolist自定义事件版本/main.js
@@ -1,12 +1,15 @@
 //引入Vue
-import Vue from 'vue'
+import Vue from "vue";
 //引入App
-import App from './App'
+import App from './App';
 
 //关闭Vue的生产提示
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
 
 new Vue({
-    el: '#app',
-    render: (h) => h(App),
-})
+    el:'#app',
+    render: h => h(App)
+});
+
+
