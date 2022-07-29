716e312cf5cd2961bc52eec3476d57275709bc14
Cicada <1547598968@qq.com>
2022-07-29 23:56:07 +0800

    插槽

---
+++ vue_show/22_插槽/01_默认插槽/components/Category.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="category">
+        <h3>{{ title }}分类</h3>
+        <!--插槽,等着组件的使用者进行填充-->
+        <slot>我是默认值</slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Category',
+    props: ['title'],
+}
+</script>
+
+<style>
+.category {
+    background-color: skyblue;
+    width: 200px;
+    height: 200px;
+}
+h3 {
+    text-align: center;
+    background: orange;
+}
+img {
+    width: 100%;
+}
+</style>
