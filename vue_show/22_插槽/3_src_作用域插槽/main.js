066015fb55fc3d566dba876e95cffb185474e006
Cicada <1547598968@qq.com>
2022-07-30 09:58:57 +0800

    插槽

--- vue_show/22_插槽/3_src_作用域插槽/main.js
+++ vue_show/22_插槽/3_src_作用域插槽/main.js
@@ -1,18 +1,19 @@
 //引入Vue
-import Vue from "vue";
-//引入vue-resource
+import Vue from 'vue'
 //引入App
-import App from './App';
-
+import App from './App.vue'
+//引入插件
+import vueResource from 'vue-resource'
 //关闭Vue的生产提示
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+//使用插件
+Vue.use(vueResource)
 
-
+//创建vm
 new Vue({
     el: '#app',
-    render: h => h(App),
+    render: (h) => h(App),
     beforeCreate() {
-        Vue.prototype.$bus = this;
-    }
-});
-
+        Vue.prototype.$bus = this
+    },
+})
